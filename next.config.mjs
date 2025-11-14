10ead718661fb07de61c623e714ef9a90c630886
ranjeetsingh-cell <ranjeet.singh@ethoswatches.com>
2025-11-14 23:01:11 +0530

    config chnages

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
    images: {
     domains: ["manage.tajwaycabs.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*{jpg,jpeg,png,svg,gif,webp,ico}",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
